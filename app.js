53e4fe2f68675a6cadfcad51f9c3d21cfbfb42a6
Alissa Renz <alissa.renz@gmail.com>
2024-09-04 15:33:50 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const { App, LogLevel } = require('@slack/bolt');
+const { config } = require('dotenv');
+const { registerListeners } = require('./listeners');
+
+config();
+
+/** Initialization */
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  logLevel: LogLevel.DEBUG,
+});
+
+/** Register Listeners */
+registerListeners(app);
+
+/** Start the Bolt App */
+(async () => {
+  try {
+    await app.start();
+    console.log('⚡️ Bolt app is running!');
+  } catch (error) {
+    console.error('Failed to start the app', error);
+  }
+})();
